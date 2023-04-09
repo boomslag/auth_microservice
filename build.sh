6856f57b83806929a18ce70ee0ed3a167943f014
SoloPython <alexander.eric.3@gmail.com>
2023-04-09 08:20:07 -0500

    specified python version in build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+# Specify the desired Python version
+PYTHON_VERSION="3.9"
+
 # Create the build/static folder if it doesn't exist
 mkdir -p build/static
 
 # Continue with the rest of the build script
-python -m venv .venv
+python${PYTHON_VERSION} -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
