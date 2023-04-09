8e9f44e0ab9dc2ce8bca01d1e20a6b2962f42335
SoloPython <alexander.eric.3@gmail.com>
2023-04-09 09:19:17 -0500

    improved build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,24 @@
 set -e
 
 # Create the build/static folder if it doesn't exist
+echo "Creating build/static folder if it doesn't exist..."
 mkdir -p build/static
 
 # Continue with the rest of the build script
+echo "Creating virtual environment..."
 python -m venv .venv
 source .venv/bin/activate
+
+echo "Upgrading pip..."
 pip install --upgrade pip
+
+echo "Installing requirements..."
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+echo "Running collectstatic..."
+python manage.py collectstatic --no-input -v 3
+
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Build completed successfully!"
