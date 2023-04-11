8bfb8e58b27d047ab5b4311c17eacd12dd11c64a
SoloPython <alexander.eric.3@gmail.com>
2023-04-11 12:48:27 -0500

    Email Delivery

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,8 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
-# DEBUG = 'RENDER' not in os.environ
+# DEBUG = os.environ.get('DEBUG')
+DEBUG = 'RENDER' not in os.environ
 DOMAIN = os.environ.get('DOMAIN')
 
 PDM_ADDRESS_GOERLI = os.environ.get('PDM_ADDRESS_GOERLI')
