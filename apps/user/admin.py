e1441712df3ac56e37b3c67686eb82f6572c556a
SoloPython <alexander.eric.3@gmail.com>
2023-04-21 16:57:32 -0500

    License

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('username','first_name', 'last_name','email', 'is_staff', 'become_seller', 'role','verified')
     list_display_links = ('username','first_name', 'last_name','email', )
     list_filter = ('username','first_name', 'last_name','email', 'is_staff', 'become_seller', 'role',)
-    search_fields = ('username','first_name', 'last_name','email','role','verified', 'become_seller', )
+    search_fields = ('id','username','first_name', 'last_name','email','role','verified', 'become_seller', )
     list_editable = ('become_seller', 'role', 'verified','is_staff' )
     list_per_page = 25
 
